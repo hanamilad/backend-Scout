6729c44ee91a5c5ffbb106b4c46326decb992dd8
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-04-22 23:38:33 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
-
 module.exports = ({ env }) => ({
-  Connection: {
-    Client: 'postgres',
-       Connection:{
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      username: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
