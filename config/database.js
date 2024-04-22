6a41977367aac38068035846262d16f785785ff5
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-04-22 23:23:41 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
+        client :env('DATABASE_CLIENT', ),
         host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
